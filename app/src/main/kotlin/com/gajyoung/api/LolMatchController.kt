6a5dc1277409d33d3fd538348918f1b7bf8606ca
Ga Jun <gajunyoung1997@gmail.com>
2024-03-15 15:39:28 +0000

    Fix Match endpoint

--- app/src/main/kotlin/com/gajyoung/api/LolMatchController.kt
+++ app/src/main/kotlin/com/gajyoung/api/LolMatchController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/vi/lol/match")
+@RequestMapping("/api/v1/lol/match")
 class LolMatchController(
     private val matchService: MatchService,
     private val leagueMatchApi: LeagueMatchApi,
